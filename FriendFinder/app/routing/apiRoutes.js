3e0504877ef255f4c647436cf2131dc3da91ec91
chaula Jinger <jingerc4@gmail.com>
2018-08-05 00:03:52 -0400

    <Submit>

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,24 @@
+// A GET route with the url /api/friends. 
+// 	This will be used to display a JSON of all possible friends.
+// A POST routes /api/friends. 
+// 	This will be used to handle incoming survey results. 
+// 	This route will also be used to handle the compatibility logic.
+
+//These routes for our data. Help determine data user sees and data can post
+
+var friendsData = require ('../data/friends.js');
+
+module.exports = function (app) {
+	app.get('/api/friends', function(req, res) {
+		res.json(friendsData);
+	});
+
+
+	app.post('/api/friends', function(req, res){
+		// if(friendsData.length<5) {
+			friendsData.push(req.body);
+			res.json(true);
+		// }
+	});
+
+}
